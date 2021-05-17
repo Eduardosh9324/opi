4c8daad64cff1f8404cd756003f0977ce1bf5dd3
eduardosahe9324 <eduardo9_3@hotmail.com>
2021-05-16 22:34:30 -0500

    test: se añaden comentarios al script del ejercicio A

--- Ejercicio A.R
+++ Ejercicio A.R
@@ -1,18 +1,18 @@
 library(sqldf)
-
+#Cargamos la base de datos
 setwd("C:/Users/lenovo/Desktop/Opi analytics/scripts")
 crimes<-read.csv(
   "carpetas-de-investigacion-pgj-cdmx.csv",
   encoding = "UTF-8",
   as.is = c("character","integer","factor","character",rep("factor",7),"character","character","integer","character","character","numeric","numeric","character"))
-#crimes <-na.omit(crimes)
 
+#Ajustamos la clase de la variable mes_hechos
 crimes$mes_hechos<-factor(
   as.character(crimes$mes_hechos),
   levels = c("Enero",  "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre", ""),
             ordered = TRUE
   )
-
+#Ajustamos la clase de la variable ao_hechos
 crimes$ao_hechos <- factor(
   as.character(crimes$ao_hechos),
   levels = as.character(sort(unique(crimes$ao_hechos))),
@@ -20,21 +20,21 @@ crimes$ao_hechos <- factor(
   )
 levels(crimes$ao_hechos)
 
-#1
+#Pregunta 1
 
 
-#2
+#Pregunta 2: Numero de registros y rango de tiempo de los registros
 dim(crimes)#808871 registros
 range(crimes$ao_hechos)
 tapply(crimes$mes_hechos, crimes$ao_hechos, min)[1]#1906_6
 tapply(crimes$mes_hechos, crimes$ao_hechos, max)#2019_6
 
-#3
+#Pregunta 3: Distribucion de los delitos y mostramos los 5 más frecuentes
 sortTable<-sort(table(crimes$delito),decreasing = T)[1:5]
 sortTable
-
 plot(x=sortTable,)
 barplot(sortTable)
+  #Respuesta del 5
 # VIOLENCIA FAMILIAR 
 # 69517 
 # ROBO DE OBJETOS 
@@ -48,32 +48,14 @@ barplot(sortTable)
 # AMENAZAS 
 # 37415 
 
-
-
-
-#3
-delitos<-as.factor(crimes$delito)
-levels(delitos)
-sort(table(delitos),decreasing = T)[1:5]
-# VIOLENCIA FAMILIAR 
-# 69517 
-# ROBO DE OBJETOS 
-# 52214 
-# ROBO A NEGOCIO SIN VIOLENCIA 
-# 51426 
-# FRAUDE 
-# 45349 
-# DENUNCIA DE HECHOS 
-# 44433
-
-#4pendiente
+#Pregunta 4: Identificando los delitos que van al alza y a la baja en el ultimo año
   #filtramos solo del 2019
 delitos_alza<-crimes[crimes$ao_hechos == 2019,]
   #definimos funcion auxiliar
 library(dplyr)
 on<-group_by(delitos_alza,ao_hechos,mes_hechos))
 
-#5
+#Pregunta 5: Encontrando la alcaldia con mas delitos y las que no tienen tanto
 sort(tapply(crimes$alcaldia_hechos, crimes$alcaldia_hechos, length))
 #con mas CUAUHTEMOC 131397
 #hay muchas alcaldias de las que se cunetan solo con un registro
@@ -112,13 +94,7 @@ sort(tapply(crimes$alcaldia_hechos, crimes$alcaldia_hechos, length))
 # AMECA 
 # 1
 
-#7
-auxFilter1<-function(arreglo){
-  aux<-sort(table(arreglo),decreasing = F)[1:3]
-  return(aux)
-}
-L<-tapply(crimes$alcaldia_hechos, crimes$alcaldia_hechos, auxFilter1)
-
+#Pregunta 6: Mostrando las 3 colonias con mas delitos por cada alcaldia
 group_alcaldia_colonia<-sqldf('SELECT alcaldia_hechos, colonia_hechos, COUNT(*) AS numberDelitos FROM crimes GROUP BY alcaldia_hechos, colonia_hechos ORDER BY numberDelitos desc')
 sqldf('select * from (
     select alcaldia_hechos, 
@@ -128,7 +104,6 @@ sqldf('select * from (
       from group_alcaldia_colonia) ranks
       where delitos_rank <= 3;')
 
-#7
 
 
 
